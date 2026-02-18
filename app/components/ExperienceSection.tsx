79d647caa1e43535a6beeee72ed74df81f29bf25
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2026-02-17 21:36:25 -0700

    Updated styling of project section.

--- app/components/ExperienceSection.tsx
+++ app/components/ExperienceSection.tsx
@@ -2,6 +2,7 @@ const ExperienceSection = () => {
 	const experiences = [
 		{
 			project: 'Friend Game Matcher',
+			image: "https://images7.alphacoders.com/133/1339892.png",
 			link: 'https://github.com/JSandhu223/game-matcher',
 			appType: 'Blazor App',
 			techStack: '.NET, Blazor',
@@ -9,6 +10,7 @@ const ExperienceSection = () => {
 		},
 		{
 			project: 'RPG Game in Unreal Engine and C++',
+			image: "https://external-preview.redd.it/g-x5gzLtseLf2AoW4cp0nhrS8V6hIQY0DYJYI5WARFw.jpg?width=640&crop=smart&auto=webp&s=ed48ec7a6e678b6ac8fe630257da83aac81ad108",
 			link: 'https://github.com/JSandhu223/unreal-rpg',
 			appType: 'Game',
 			techStack: 'Unreal Engine 5, C++',
@@ -16,6 +18,7 @@ const ExperienceSection = () => {
 		},
 		{
 			project: 'Collaboration App for Artists',
+			image: "https://raw.githubusercontent.com/J-essicaTran/inklink/refs/heads/main/public/logo.png",
 			link: 'https://github.com/J-essicaTran/inklink',
 			appType: 'Web App',
 			techStack: 'NextJS, React, Typescript, GCP',
@@ -31,22 +34,19 @@ const ExperienceSection = () => {
 					<div className="h-px w-full bg-gray-200 dark:bg-gray-700 mt-2" />
 				</div>
 
-				<div className="space-y-5">
+				<div className="grid grid-cols-2 gap-2">
 					{experiences.map((exp) => (
-						<div key={exp.appType} className="relative">
-							<div className="relative pl-4 border-l-2 border-gray-200 dark:border-gray-700">
-								<div className="absolute -left-[5px] top-[10px] h-2.5 w-2.5 rounded-full bg-gray-300 dark:bg-gray-600" />
-
-								<div className="space-y-1">
-									<div className="flex flex-col md:flex-row md:items-center md:justify-between">
-										{/* <h4 className="text-md font-medium text-gray-900 dark:text-white">{exp.project}</h4> */}
-										<a href={exp.link} target="_blank" className="text-lg font-medium text-gray-900 dark:text-white hover:underline">{exp.project}</a>
-										<span className="text-sm text-gray-500 dark:text-gray-400">{exp.techStack}</span>
-									</div>
-									<p className="text-sm font-medium text-gray-600 dark:text-gray-300">{exp.appType}</p>
-									<p className="text-sm text-gray-600 dark:text-gray-400 leading-relaxed">{exp.description}</p>
-								</div>
-							</div>
+						<div key={exp.appType} className="w-full border-solid border-2 border-white px-5 pb-2 rounded-md">
+								{/* <div className="absolute -left-[5px] top-[10px] h-2.5 w-2.5 rounded-full bg-gray-300 dark:bg-gray-600" /> */}
+								{/* <h4 className="text-md font-medium text-gray-900 dark:text-white">{exp.project}</h4> */}
+								<a href={exp.link} target="_blank" className="flex flex-col justify-center items-center text-lg font-medium text-gray-900 dark:text-white hover:underline">
+									<img className="w-fit" src={exp.image}></img>
+									<p>{exp.project}</p>
+								</a>
+								<span className="flex flex-row-reverse text-sm italic text-gray-500 dark:text-gray-400">{exp.techStack}</span>
+								<div></div>
+								{/* <p className="text-sm font-medium text-gray-600 dark:text-gray-300">{exp.appType}</p> */}
+								<p className="text-sm text-gray-600 dark:text-gray-400 leading-relaxed">{exp.description}</p>
 						</div>
 					))}
 				</div>
