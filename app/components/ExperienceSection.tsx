46f38a4fc8aa71c3ce2d3e46dcfe91692ee364f8
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2026-02-18 16:42:33 -0700

    Changed projects section from being in a grid a flexbox.

--- app/components/ExperienceSection.tsx
+++ app/components/ExperienceSection.tsx
@@ -9,7 +9,7 @@ const ExperienceSection = () => {
 			description: 'A web app for helping friend groups (or strangers) looking to find common game interests. Uses the Steam Web API to collect user profiles data.',
 		},
 		{
-			project: 'RPG Game in Unreal Engine and C++',
+			project: 'RPG in Unreal Engine',
 			image: "https://external-preview.redd.it/g-x5gzLtseLf2AoW4cp0nhrS8V6hIQY0DYJYI5WARFw.jpg?width=640&crop=smart&auto=webp&s=ed48ec7a6e678b6ac8fe630257da83aac81ad108",
 			link: 'https://github.com/JSandhu223/unreal-rpg',
 			appType: 'Game',
@@ -34,19 +34,34 @@ const ExperienceSection = () => {
 					<div className="h-px w-full bg-gray-200 dark:bg-gray-700 mt-2" />
 				</div>
 
-				<div className="grid grid-cols-2 gap-2">
-					{experiences.map((exp) => (
-						<div key={exp.appType} className="w-full border-solid border-2 border-white px-5 pb-2 rounded-md">
+				{/* <div className="grid grid-cols-2 gap-2"> */}
+					{/* {experiences.map((exp) => ( */}
+						{/* <div key={exp.appType} className="h-96 border-solid border-2 border-white px-5 pb-2 rounded-md"> */}
 								{/* <div className="absolute -left-[5px] top-[10px] h-2.5 w-2.5 rounded-full bg-gray-300 dark:bg-gray-600" /> */}
 								{/* <h4 className="text-md font-medium text-gray-900 dark:text-white">{exp.project}</h4> */}
-								<a href={exp.link} target="_blank" className="flex flex-col justify-center items-center text-lg font-medium text-gray-900 dark:text-white hover:underline">
-									<img className="w-fit" src={exp.image}></img>
-									<p>{exp.project}</p>
-								</a>
-								<span className="flex flex-row-reverse text-sm italic text-gray-500 dark:text-gray-400">{exp.techStack}</span>
-								<div></div>
+								{/* <a href={exp.link} target="_blank" className="flex flex-col justify-center items-center text-lg font-medium text-gray-900 dark:text-white hover:underline"> */}
+									{/* <img className="w-fit h-5/6" src={exp.image}></img> */}
+									{/* <p>{exp.project}</p> */}
+								{/* </a> */}
+								{/* <span className="flex flex-row-reverse text-sm italic text-gray-500 dark:text-gray-400">{exp.techStack}</span> */}
+								{/* <div></div> */}
 								{/* <p className="text-sm font-medium text-gray-600 dark:text-gray-300">{exp.appType}</p> */}
-								<p className="text-sm text-gray-600 dark:text-gray-400 leading-relaxed">{exp.description}</p>
+								{/* <p className="text-sm text-gray-600 dark:text-gray-400 leading-relaxed">{exp.description}</p> */}
+						{/* </div> */}
+					{/* ))} */}
+				{/* </div> */}
+
+				<div className="flex flex-wrap justify-start gap-6">
+					{experiences.map((exp) => (
+						<div key={exp.appType} className="border-solid border-2 border-white rounded-md overflow-hidden w-full flex flex-row">
+							<a href={exp.link} target="_blank" className="hover:bg-sky-700">
+								<div className="p-4">
+									{/* <img className="" src={exp.image}></img> */}
+									<h2 className="dark: text-white">{exp.project}</h2>
+									<p className="text-sm italic text-black dark:text-white leading-relaxed">{exp.techStack}</p>
+									<p className="mt-auto mb-auto text-sm text-gray-600 dark:text-gray-400 leading-relaxed">{exp.description}</p>
+								</div>
+							</a>
 						</div>
 					))}
 				</div>
